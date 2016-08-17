8338ba117a98e4eec79a23e0aa0207afd74d9188
Hannah Marlin <hmmarlin@gmail.com>
2016-08-17 13:35:09 -0700

    added current time clock, added basic form validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+ // add firebase config  
   var config = {
       apiKey: "AIzaSyDMjNwn0OA3jmqHUU2tE1AS3-eqaoZbYSg",
       authDomain: "train-game-2fcfd.firebaseapp.com",
@@ -8,7 +9,13 @@
 
   var database = firebase.database();
 
+// display the current time on the train schedule
+  var timeNow = moment().format("HH:mm");
+  $(document).ready(function() {
+      $("#timeNow").append(timeNow);
+});
 
+// create functionality to add a new train to the board on click of "submit" button
   $("#addTrain").on("click", function() {
 
       var trainName = $("#trainNameInput").val().trim();
@@ -16,9 +23,29 @@
       var trainTime = moment($("#firstTimeInput").val().trim(), "HH:mm").format("LT");
       var trainFreq = $("#frequencyInput").val().trim();
 
-      console.log(trainName + trainTime);
+// validat data to prevent users from entering null information
+    if (trainName == null || trainName == "") {
+        alert("Please add a name for the train");
+        return false;
+    }
 
+      else if (trainDest == null || trainDest == "") {
+        alert("Please add a destination for the train");
+        return false;
+    }
+      else if (trainTime == null || trainTime == "") {
+        alert("Please add a departure time for the train");
+        return false;
+    }
 
+    else if (trainFreq == null || trainFreq == "") {
+        alert("Please add a frequency for the train");
+        return false;
+    }
+
+    else {
+
+// functionality to create new object to capture the user data. 
       var newTrain = {
           train: trainName,
           destination: trainDest,
@@ -36,9 +63,10 @@
       $("#frequencyInput").val("");
 
       return false;
+    }
   });
 
-
+// capture the data in firebase and append it to the main train table. 
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
       var trainName = childSnapshot.val().train;
@@ -46,7 +74,7 @@
       var trainTime = childSnapshot.val().time;
       var trainFreq = childSnapshot.val().frequency;
 
-      var tFrequency = trainFreq;
+      var trainFrequency = trainFreq;
       var firstTime = trainTime;
 
       var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
@@ -55,13 +83,14 @@
 
       var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-      var tRemainder = diffTime % tFrequency;
+      var tRemainder = diffTime % trainFrequency;
 
-      var tMinutesTillTrain = tFrequency - tRemainder;
+      var tMinutesTillTrain = trainFrequency - tRemainder;
 
       var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm")
-
 
       $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain + "</td></tr>");
 
   });
+
+
